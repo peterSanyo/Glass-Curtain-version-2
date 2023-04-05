b4d534e3f532416fd1688c6c1aa4908b878ff8e9
peterSanyo <peter.sanyo@code.berlin>
2023-04-05 23:21:56 +0200

    cleaned up app structure

--- app/app.py
+++ app/app.py
@@ -1,118 +1,16 @@
 from flask import Flask, redirect, url_for, render_template, send_file
+from . import pieces
 
-pieces = {
-    "era300" : {
-        "name": "Era 300",
-        "image_url": "https://stadt-bremerhaven.de/wp-content/uploads/2023/02/Sonos-Era-300-Credenza-3.jpg",
-        "quote": ' "The most suffisticated product Sonos has built" ',
-        "quoted":"Dana Krieger, head of Sonos",
-        "offerer":"Sonos",
-        "dimensions": "30x20x10 cm",
-        "materials": "hard case plastic and wood",
-        "category":"Spatial home Audio",
-        "price": "399$",
-        "count": 1 
-    },
-    "phantom_syntopia" : {
-        "name": "Phantom Syntopia",
-        "image_url": "https://cdn.motor1.com/images/mgl/g4JVP9/s1/rolls-royce-phantom-syntopia.webp",
-        "quote": ' "a state-of-the-art experience [...] by the forces of nature." ',
-        "quoted":"Iris van Herpen",
-        "offerer":"Rolls Royce X Iris van Herpen",
-        "dimensions": "571x214x157 cm",
-        "materials": "Leather, Fabric, Steel",
-        "category":"Vehicle",
-        "price": "-",
-        "count": 2
-    },
-    "white_fromme" : {
-        "name": "White Fromme Collection",
-        "image_url": "https://static.dezeen.com/uploads/2023/03/fromme-petite-friture-tom-chung_dezeen_2364_col_3-1704x959.jpg",
-        "quote": ' "This design was inspired by the personal story of Tom Chung." ',
-        "quoted":"Petite Friture, French Design Brand",
-        "offerer":"Petite Friture X Tom Chang",
-        "dimensions": "-",
-        "materials": "Hardwearing Aluminium",
-        "category":"Exclusive Furniture",
-        "price": "-",
-        "count": 3
-    },
-    "color_palette" : {
-        "name": "2023 Color Palette",
-        "image_url": "https://static.dezeen.com/uploads/2023/02/2023-color-collection-3form-design_dezeen_2364_hero2-1704x959.jpg",
-        "quote": ' "This collection allowed us to be more introspective about the meaning of colour in our lives." ',
-        "quoted":"Ryan Smith, chief creative of 3form",
-        "offerer":"3form",
-        "dimensions": "-",
-        "materials": "Varia, Chroma and Glass",
-        "category":"Modular Interiour Design ",
-        "price": "-",
-        "count": 4
-    }
-}
 
-app = Flask(__name__)
-app.config.from_object('config')
 
-@app.route('/')
-def index():
-    pieces = {
-    "era300" : {
-        "name": "Era 300",
-        "image_url": "https://stadt-bremerhaven.de/wp-content/uploads/2023/02/Sonos-Era-300-Credenza-3.jpg",
-        "quote": ' "The most suffisticated product Sonos has built" ',
-        "quoted":"Dana Krieger, head of Sonos",
-        "offerer":"Sonos",
-        "dimensions": "30x20x10 cm",
-        "materials": "hard case plastic and wood",
-        "category":"Spatial home Audio",
-        "price": "399$",
-        "count": 1 
-    },
-    "phantom_syntopia" : {
-        "name": "Phantom Syntopia",
-        "image_url": "https://cdn.motor1.com/images/mgl/g4JVP9/s1/rolls-royce-phantom-syntopia.webp",
-        "quote": ' "a state-of-the-art experience [...] by the forces of nature." ',
-        "quoted":"Iris van Herpen",
-        "offerer":"Rolls Royce X Iris van Herpen",
-        "dimensions": "571x214x157 cm",
-        "materials": "Leather, Fabric, Steel",
-        "category":"Vehicle",
-        "price": "-",
-        "count": 2
-    },
-    "white_fromme" : {
-        "name": "White Fromme Collection",
-        "image_url": "https://static.dezeen.com/uploads/2023/03/fromme-petite-friture-tom-chung_dezeen_2364_col_3-1704x959.jpg",
-        "quote": ' "This design was inspired by the personal story of Tom Chung." ',
-        "quoted":"Petite Friture, French Design Brand",
-        "offerer":"Petite Friture X Tom Chang",
-        "dimensions": "-",
-        "materials": "Hardwearing Aluminium",
-        "category":"Exclusive Furniture",
-        "price": "-",
-        "count": 3
-    },
-    "color_palette" : {
-        "name": "2023 Color Palette",
-        "image_url": "https://static.dezeen.com/uploads/2023/02/2023-color-collection-3form-design_dezeen_2364_hero2-1704x959.jpg",
-        "quote": ' "This collection allowed us to be more introspective about the meaning of colour in our lives." ',
-        "quoted":"Ryan Smith, chief creative of 3form",
-        "offerer":"3form",
-        "dimensions": "-",
-        "materials": "Varia, Chroma and Glass",
-        "category":"Modular Interiour Design ",
-        "price": "-",
-        "count": 4
-    }
-}
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('app.config')
 
-    pieces=pieces
-    return render_template("index.html", pieces=pieces)
+    register_blueprints(app)
 
-@app.route("/download")
-def download():
-    return send_file("static/downloads/exhibition.txt", as_attachment=True)
+    return app
 
-if __name__ == "__main__":
-    app.run()
+# Blueprints
+def register_blueprints(app: Flask):
+    app.register_blueprint(pieces.routes.blueprint)
