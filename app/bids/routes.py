6363c6e08a62954c358663d7623623f2e642295b
peterSanyo <peter.sanyo@code.berlin>
2023-04-10 19:35:00 +0200

    validating incoming form data

--- app/bids/routes.py
+++ app/bids/routes.py
@@ -14,6 +14,18 @@ def get_checkout():
 def post_checkout():
     pieces = Piece.query.all()
 
-    create_bid(request.form, pieces)
+    if not all([
+        request.form.get("user_name"),
+        request.form.get("country_of_origin"),
+        request.form.get("user_email"),
+        request.form.get("piece_id"),
+        request.form.get("amount_of_bid"),
+        request.form.get("letter")
+    ]):
+        return render_template("bids/new.html",
+        piece=pieces,
+        error = "Please fill out the whole form to place a valid bid."
+        )
 
+    create_bid(request.form, pieces)
     return render_template("bids/new.html", piece=pieces)
