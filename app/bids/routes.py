15a1e2566ce8ff6acd11c6d12a5e158130bd847a
peterSanyo <peter.sanyo@code.berlin>
2023-04-10 19:45:48 +0200

    Logging Errors and exceptions built into post_checkout()

--- app/bids/routes.py
+++ app/bids/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, current_app
 from app.pieces.models import Piece
 from .services.create_bid import create_bid
 
@@ -12,20 +12,28 @@ def get_checkout():
 
 @blueprint.post("/checkout")
 def post_checkout():
-    pieces = Piece.query.all()
+    try:
+        pieces = Piece.query.all()
 
-    if not all([
-        request.form.get("user_name"),
-        request.form.get("country_of_origin"),
-        request.form.get("user_email"),
-        request.form.get("piece_id"),
-        request.form.get("amount_of_bid"),
-        request.form.get("letter")
-    ]):
+        if not all([
+            request.form.get("user_name"),
+            request.form.get("country_of_origin"),
+            request.form.get("user_email"),
+            request.form.get("piece_id"),
+            request.form.get("amount_of_bid"),
+            request.form.get("letter")
+        ]):
+            raise Exception("Please fill out all fields.")
+
+        create_bid(request.form, pieces)
+        return render_template("bids/new.html", piece=pieces)
+    
+    except Exception as error_message:
+        error = error_message or "An error occured while processing your order. Please make sure to enter valid data."
+        
+        current_app.logger.info(f"Error creating a bid: {error}")
+
         return render_template("bids/new.html",
         piece=pieces,
-        error = "Please fill out the whole form to place a valid bid."
+        error=error
         )
-
-    create_bid(request.form, pieces)
-    return render_template("bids/new.html", piece=pieces)
