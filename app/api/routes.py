0e8a24dcde91590c81823f41518c5b959b310759
peterSanyo <peter.sanyo@code.berlin>
2023-04-11 11:59:54 +0200

    implemented api endpoints with versioning and RESTful naming

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,14 +1,17 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from .services.serialize_bids import serialize_bids
 from ..bids.models import Bid
+from os import environ
 
 blueprint = Blueprint("api", __name__)
 
 @blueprint.get("/api/v1/bids")
 def bids():
-    bids=Bid.query.all()
-
-    return jsonify(
-        serialize_bids(bids)
-    )
+    if environ.get("API_KEY") == request.args.get("key"):
+        bids=Bid.query.all()
+        return jsonify(
+            serialize_bids(bids)
+        )
+    else:
+        return jsonify({"error": "Invalid API key"}), 401
 
