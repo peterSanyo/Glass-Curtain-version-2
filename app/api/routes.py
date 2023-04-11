21f889b83129127f5884c7aeca211a261830e8c1
peterSanyo <peter.sanyo@code.berlin>
2023-04-11 11:08:48 +0200

    corrected naming of api routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,9 +1,14 @@
 from flask import Blueprint, jsonify
+from .services.serialize_bids import serialize_bids
+from ..bids.models import Bid
 
 blueprint = Blueprint("api", __name__)
 
 @blueprint.get("/api/v1/bids")
 def bids():
+    bids=Bid.query.all()
+
     return jsonify({
-        "data" : "Hello World"
+        serialize_bids(bids)
     })
+
