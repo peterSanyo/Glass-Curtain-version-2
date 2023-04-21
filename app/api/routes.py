3c6a9e9ab0b1d41475548a7fc0c3ef4217d1dba0
peterSanyo <peter.sanyo@code.berlin>
2023-04-21 13:20:45 +0200

    added listview to api acess

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,17 +1,19 @@
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, request, render_template
 from .services.serialize_bids import serialize_bids
 from ..bids.models import Bid
 from os import environ
 
 blueprint = Blueprint("api", __name__)
 
+
 @blueprint.get("/api/v1/bids")
 def bids():
     if environ.get("API_KEY") == request.args.get("key"):
         bids=Bid.query.all()
-        return jsonify(
+        incomin_bids = jsonify(
             serialize_bids(bids)
-        )
+            )
+        return render_template("bids/api.html", incoming_bids=incomin_bids)
     else:
         return jsonify({"error": "Invalid API key"}), 401
 
