2cd8d94175b677792abe5681957e13d7cb3b962a
peterSanyo <peter.sanyo@code.berlin>
2023-04-24 14:47:44 +0200

    bug fixing the seed routes

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,27 +1,45 @@
-from flask import Blueprint, jsonify, request, render_template, redirect, url_for, current_app
+from flask import Blueprint, jsonify, request, render_template, redirect, url_for, current_app, render_template_string
 from .services.serialize_bids import serialize_bids
 from ..bids.models import Bid
 from ..pieces.models import Piece
 from app.extensions.database import db
+from os import environ
 
 blueprint = Blueprint("api", __name__)
 
 
 @blueprint.get("/api/v1/admin")
 def bids():
-    # if environ.get("API_KEY") == request.args.get("key"):
+    if environ.get("API_KEY") == request.args.get("key"):
         bids = Bid.query.all()
         serialized_bids = serialize_bids(bids) 
         return render_template("bids/api.html", incoming_bids=serialized_bids)
-    # else:
-    #     return jsonify({"error": "Invalid API key"}), 401
+    else:
+         return jsonify({"error": "Invalid API key"}), 401
     
-    
+@blueprint.route("/run-seed")
+# run seeds !
+def run_seed():
+    if not Piece.query.first():
+        import app.scripts.seed
+        return render_template_string("DATABASE SEED COMPLETED <a href='/'>Back to Gallery</a> \n \n <a href='/api/v1/admin'>To Admin Page</a>")
+    else:
+        return render_template_string("NOTHING TO RUN! <a href='/'>Back to Gallery</a> \n \n <a href='/api/v1/admin'>To Admin Page</a>")
+
+
+@blueprint.route("/delete-seed")
+def delete_seed():
+    if not Piece.query.first():
+        return render_template_string("Nothing to delete here \n <a href='/'>Back to Gallery</a> \n \n <a href='/api/v1/admin'>To Admin Page</a>")
+    else:
+        db.session.query(Piece).delete()
+        db.session.commit()
+        return render_template_string("SEEDS Deleted <a href='/'>Back to Gallery</a> \n \n <a href='/api/v1/admin'>To Admin Page</a>")
 
 
 @blueprint.route("/api/v1/admin", methods=["GET", "POST"])
 def update_pieces():
-    # if environ.get("API_KEY") == request.args.get("key"):
+    if environ.get("API_KEY") == request.args.get("key"):
         if request.method == "POST":
             # Delete old Piece objects
             old_pieces = Piece.query.all()
--- app/pieces/routes.py
+++ app/pieces/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, send_file, render_template_string, current_app
+from flask import Blueprint, render_template, send_file
 from .models import Piece
 from app.extensions.database import db
 
@@ -23,21 +23,3 @@ def download():
     # download exhibit material
     return send_file("static/downloads/exhibition.txt", as_attachment=True)
 
-@blueprint.route("/run-seed")
-# run seeds !
-def run_seed():
-    if not Piece.query.first():
-        import app.scripts.seed
-        return render_template_string("DATABASE SEED COMPLETED <a href='/'>Back to Gallery</a> \n \n <a href='/api/v1/admin'>To Admin Page</a>")
-    else:
-        return render_template_string("NOTHING TO RUN! <a href='/'>Back to Gallery</a> \n \n <a href='/api/v1/admin'>To Admin Page</a>")
-
-
-@blueprint.route("/delete-seed")
-def delete_seed():
-    if not Piece.query.first():
-        return render_template_string("Nothing to delete here \n <a href='/'>Back to Gallery</a> \n \n <a href='/api/v1/admin'>To Admin Page</a>")
-    else:
-        db.session.query(Piece).delete()
-        db.session.commit()
-        return render_template_string("SEEDS Deleted <a href='/'>Back to Gallery</a> \n \n <a href='/api/v1/admin'>To Admin Page</a>")
